9139d146e1b25220e4b4301976c9a1b61700dc2e
98.amirhosein <98.amirhosein@gmail.com>
2020-02-26 13:06:15 +0330

    first commit

--- src/com/company/searchEngine.java
+++ src/com/company/searchEngine.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class searchEngine {
     String files[];
 
